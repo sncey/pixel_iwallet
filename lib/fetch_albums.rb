61f2a2bae84249fcae589d2b87d4afdb51634cef
sncey <ceyda666esen@gmail.com>
2024-06-20 23:56:16 +0300

    Define FetchAlbums class in fetch_albums.rb

--- lib/fetch_albums.rb
+++ lib/fetch_albums.rb
@@ -3,36 +3,39 @@
 require 'net/http'
 require 'json'
 
-# JSONPlaceholder endpoints
-albums_uri = URI('https://jsonplaceholder.typicode.com/albums')
-photos_uri = URI('https://jsonplaceholder.typicode.com/photos')
+class FetchAlbums
+  JSONPLACEHOLDER_ALBUMS_URI = URI('https://jsonplaceholder.typicode.com/albums')
+  JSONPLACEHOLDER_PHOTOS_URI = URI('https://jsonplaceholder.typicode.com/photos')
 
-# HTTP GET requests
-albums_response = Net::HTTP.get(albums_uri)
-photos_response = Net::HTTP.get(photos_uri)
+  def self.call
+    # HTTP GET requests
+    albums_response = Net::HTTP.get(JSONPLACEHOLDER_ALBUMS_URI)
+    photos_response = Net::HTTP.get(JSONPLACEHOLDER_PHOTOS_URI)
 
-# Parse JSON responses
-albums = JSON.parse(albums_response)
-photos = JSON.parse(photos_response)
+    # Parse JSON responses
+    albums = JSON.parse(albums_response)
+    photos = JSON.parse(photos_response)
 
-# Hash to store user IDs and corresponding albums
-user_albums = Hash.new { |hash, key| hash[key] = [] }
+    # Hash to store user IDs and corresponding albums
+    user_albums = Hash.new { |hash, key| hash[key] = [] }
 
-# Group albums by user ID
-albums.each do |album|
-  user_albums[album['userId']] << album['id']
-end
+    # Group albums by user ID
+    albums.each do |album|
+      user_albums[album['userId']] << album['id']
+    end
 
-# Matching photos with albums
-matched_albums = []
+    # Matching photos with albums
+    matched_albums = []
 
-photos.each do |photo|
-  if user_albums.any? { |_, album_ids| album_ids.include?(photo['albumId']) }
-    matched_albums << photo
+    photos.each do |photo|
+      if user_albums.any? { |_, album_ids| album_ids.include?(photo['albumId']) }
+        matched_albums << photo
+      end
+    end
+
+    # Output matched albums
+    matched_albums.each do |matched_album|
+      puts "Album ID: #{matched_album['albumId']}, Photo Title: #{matched_album['title']}"
+    end
   end
-end
-
-# Output matched albums
-matched_albums.each do |matched_album|
-  puts "Album ID: #{matched_album['albumId']}, Photo Title: #{matched_album['title']}"
 end
